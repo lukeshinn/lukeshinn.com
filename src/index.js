b13f8c19c6fdd719479fcfa4d4769832f548dc89
lukeshinn <luke.shinn.efp@gmail.com>
2021-04-13 10:27:00 -0500

    add intro and revealer js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom'
 import Message from './js/Message'
 import NavBar from './js/NavBar'
+import './js/revealer.js'
+import './js/anime.js'
 
 import './css/style.css'
+import './scss/revealer.scss'
 
 ReactDOM.render(
-  <>
-    <NavBar />
-    <Message />
-  </>,
+    <Message />,
   document.getElementById('react-container') // eslint-disable-line no-undef
 )
+
 
 if(module.hot) // eslint-disable-line no-undef  
   module.hot.accept() // eslint-disable-line no-undef  
--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -1,15 +1,5 @@
-//import path from 'path'
-//import express from 'express'
-
-//========= Scotch Article =========
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const { ExpressOIDC } = require('@okta/oidc-middleware');
-//========= Scotch Article =========
+import path from 'path'
+import express from 'express'
 
 const app = express(),
             DIST_DIR = __dirname,
@@ -18,42 +8,9 @@ const app = express(),
 
 app.use(express.static(DIST_DIR))
 
-//app.get('*', (req, res) => {
-    //res.sendFile(HTML_FILE)
-//})
-
-
-//========= Scotch Article =========
-app.use(session({
-    secret: process.env.RANDOM_SECRET_WORD,
-    resave: true,
-    saveUninitialized: false
-}));
-
-const oidc = new ExpressOIDC({
-    issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
-    client_id: process.env.OKTA_CLIENT_ID,
-    client_secret: process.env.OKTA_CLIENT_SECRET,
-    redirect_uri: process.env.REDIRECT_URL,
-    scope: 'openid profile',
-    routes: {
-        callback: {
-            path: '/authorization-code/callback',
-            defaultRedirect: '/admin'
-        }
-    }
-});
-
-// ExpressOIDC will attach handlers for the /login and /authorization-code/callback routes
-app.use(oidc.router);
-app.use(cors());
-app.use(bodyParser.json());
-//========= Scotch Article =========
-
-app.get('/home', (req, res) => {
-  res.send('<h1>Welcome!!</h1>');
-});
-
+app.get('*', (req, res) => {
+    res.sendFile(HTML_FILE)
+})
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
