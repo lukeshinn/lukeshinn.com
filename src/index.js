42f4331c2bc073d312f97fadd1b6b77f13ef55e3
lukeshinn <luke.shinn.efp@gmail.com>
2021-04-13 18:20:07 -0500

    remove some unused code and update about section

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom'
-import Message from './js/Message'
-import NavBar from './js/NavBar'
-import './js/revealer.js'
-//import './js/anime.js'
+import About from './js/About'
 
-import './css/style.css'
+import './js/revealer.js'
 import './scss/revealer.scss'
 
 ReactDOM.render(
-    <Message />,
+    <About />,
   document.getElementById('react-container') // eslint-disable-line no-undef
 )
-
 
 if(module.hot) // eslint-disable-line no-undef  
   module.hot.accept() // eslint-disable-line no-undef  
--- src/js/About.js
+++ src/js/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Message = () => {
+const About = () => {
     return (
         <div className="content">
         <div className="section mt-1">
@@ -14,18 +14,18 @@ const Message = () => {
         <h1>Adventure</h1>
         <div className="pilot bg-img"></div>
         <p>
-        I was raised on hard work in a midwest family. Here is a picture of me and my mother when I was a child. She was so great to me! Karen inevitably died of cancer last year. Cancer plays a tough role in my life and I want to help others who are struggling
+        Recently, I have enrolled in flight school. It has always been a dream of mine to own and fly my own personal airplane. I enjoy the responsibility that comes with being a pilot. The training is strict but once we are up in the air its smooth sailing for me!  
         </p>
         </div>
         <div className="section mt-1">
         <h1>Culture</h1>
         <div className="culture bg-img"></div>
         <p>
-        I was raised on hard work in a midwest family. Here is a picture of me and my mother when I was a child. She was so great to me! Karen inevitably died of cancer last year. Cancer plays a tough role in my life and I want to help others who are struggling
+        Here is a picture of me at our company costume contest while I was working at Firespring. I always get excited for events like this. Where I can connect with my team members and get to know them on a personal level.
         </p>
         </div>
         </div>
     )
 }
 
-export default Message
+export default About
