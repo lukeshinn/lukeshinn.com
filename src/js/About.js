5f477c691313218ab815ff6c37fdd332a855243c
lukeshinn <luke.shinn.efp@gmail.com>
2021-04-14 07:56:22 -0500

    add a bit of polish, here we go

--- src/js/About.js
+++ src/js/About.js
@@ -3,24 +3,25 @@ import React from 'react'
 const About = () => {
     return (
         <div className="content">
-        <div className="section mt-1">
+        <a href="https://github.com/lukeshinn/lukeshinn.com" className="btn btn-git mt-2">Code for this Site</a>
+        <div className="section mt-2">
         <h1>Family</h1>
         <div className="momma bg-img"></div>
-        <p>
+        <p className="mt-2">
         I was raised on hard work in a midwest family. Here is a picture of me and my mother when I was a child. She was so great to me! Karen inevitably died of cancer last year. Cancer plays a tough role in my life and I want to help others who are struggling
         </p>
         </div>
-        <div className="section mt-1">
+        <div className="section mt-2">
         <h1>Adventure</h1>
         <div className="pilot bg-img"></div>
-        <p>
+        <p className="mt-2">
         Recently, I have enrolled in flight school. It has always been a dream of mine to own and fly my own personal airplane. I enjoy the responsibility that comes with being a pilot. The training is strict but once we are up in the air its smooth sailing for me!  
         </p>
         </div>
-        <div className="section mt-1">
+        <div className="section mt-2">
         <h1>Culture</h1>
         <div className="culture bg-img"></div>
-        <p>
+        <p className="mt-2">
         Here is a picture of me at our company costume contest while I was working at Firespring. I always get excited for events like this. Where I can connect with my team members and get to know them on a personal level.
         </p>
         </div>
